f874aad87004e215a4651540eac8a64f588d37a6
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-04-15 23:29:58 +0200

    Create `Models` package

--- Packages/Feature/Package.swift
+++ Packages/Feature/Package.swift
@@ -10,10 +10,13 @@ let package = Package(
         .macOS(.v13)
     ],
     products: [
+        // Core
         .library(name: "HTTPClient", targets: ["HTTPClient"]),
+        .library(name: "Models", targets: ["Models"]),
     ],
     targets: [
         .target(name: "HTTPClient"),
+        .target(name: "Models"),
         //.testTarget(name: "FeatureTests", dependencies: ["Feature"]),
     ]
 )
