00fd75d453a6d65370b0cd8fc03b57e83d9aee22
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-04-16 01:15:30 +0200

    Add test plan

--- XMSurvey/XMSurveyApp.swift
+++ XMSurvey/XMSurveyApp.swift
@@ -10,7 +10,9 @@ import App
 
 @main
 struct XMSurveyApp: App {
+    #if os(iOS)
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
     
     var body: some Scene {
         WindowGroup {
@@ -19,6 +21,7 @@ struct XMSurveyApp: App {
     }
 }
 
+#if os(iOS)
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         guard ProcessInfo.processInfo.arguments.contains("–uitesting") else { return true }
@@ -26,3 +29,4 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         return true
     }
 }
+#endif
