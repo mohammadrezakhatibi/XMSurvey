e123cb533278bf2c2f13c2deebeb279730a9aa5f
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-04-15 23:33:04 +0200

    Create `Survey` feature

--- Packages/Feature/Package.swift
+++ Packages/Feature/Package.swift
@@ -14,11 +14,18 @@ let package = Package(
         .library(name: "HTTPClient", targets: ["HTTPClient"]),
         .library(name: "Models", targets: ["Models"]),
         .library(name: "Shared", targets: ["Shared"]),
+        
+        // Features
+        .library(name: "Survey", targets: ["Survey"]),
     ],
     targets: [
+        // Core
         .target(name: "HTTPClient"),
         .target(name: "Models"),
         .target(name: "Shared"),
+        
+        // Features
+        .target(name: "Survey"),
         //.testTarget(name: "FeatureTests", dependencies: ["Feature"]),
     ]
 )
