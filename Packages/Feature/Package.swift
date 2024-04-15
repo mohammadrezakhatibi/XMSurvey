177b618f42f1a2fdb8cd7090b0daea3c6650441d
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-04-15 23:25:10 +0200

    Add platforms

--- Packages/Feature/Package.swift
+++ Packages/Feature/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Feature",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
         .library(name: "HTTPClient", targets: ["HTTPClient"]),
     ],
