2f499def821f1f246b41cb2ace7e5a63f3642317
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-04-15 23:17:20 +0200

    Initial Commit

---
+++ XMSurvey/XMSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMSurveyApp.swift
+//  XMSurvey
+//
+//  Created by Mohammadreza Khatibi on 15.04.24.
+//
+
+import SwiftUI
+
+@main
+struct XMSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
