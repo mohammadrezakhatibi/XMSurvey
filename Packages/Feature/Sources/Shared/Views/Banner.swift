336205c42d0491bf69928cb466646b80bf12dfff
Mohammadreza Khatibi <mohammadreza.khatibi@check24.de>
2024-04-16 22:37:44 +0200

    Improve Banner

--- Packages/Feature/Sources/Shared/Views/Banner.swift
+++ Packages/Feature/Sources/Shared/Views/Banner.swift
@@ -23,15 +23,14 @@ public struct Banner: ViewModifier {
     }
 
     public func body(content: Content) -> some View {
-        ZStack {
+        ZStack(alignment: .top) {
             content
-            VStack {
+            if isPresented {
                 snackBar
                     .padding(.horizontal)
-                Spacer()
             }
-            .animation(.default, value: isPresented)
         }
+        .animation(.default, value: isPresented)
         .onDisappear {
             isPresented = false
         }
@@ -39,38 +38,36 @@ public struct Banner: ViewModifier {
 
     private var snackBar: some View {
         VStack {
-            if isPresented {
-                HStack {
-                    title
-                    Spacer()
-                    if action != nil {
-                        Button {
-                            isPresented = false
-                            action?()
-                        } label: {
-                            Text("Retry")
-                        }
-                        .buttonStyle(.bordered)
+            HStack {
+                title
+                Spacer()
+                if action != nil {
+                    Button {
+                        isPresented = false
+                        action?()
+                    } label: {
+                        Text("Retry")
                     }
+                    .buttonStyle(.bordered)
                 }
-                .foregroundStyle(.white)
-                .padding()
-                .background(kind == .success ? .green : .red)
-                .opacity(isPresented ? 1 : 0)
-                .transition(.opacity)
-                .cornerRadius(8)
-                .onAppear {
-                    Task { @MainActor in
-                        try? await Task.sleep(for: .seconds(3))
-                        self.isPresented = false
-                    }
-                }
-                .onTapGesture {
-                    isPresented = false
-                }
-                .accessibilityElement(children: .contain)
-                .accessibilityIdentifier("Banner-\(kind.rawValue)")
             }
+            .foregroundStyle(.white)
+            .padding()
+            .background(kind == .success ? .green : .red)
+            .opacity(isPresented ? 1 : 0)
+            .transition(.opacity)
+            .cornerRadius(8)
+            .onAppear {
+                Task { @MainActor in
+                    try? await Task.sleep(for: .seconds(2))
+                    self.isPresented = false
+                }
+            }
+            .onTapGesture {
+                isPresented = false
+            }
+            .accessibilityElement(children: .contain)
+            .accessibilityIdentifier("Banner-\(kind.rawValue)")
         }
     }
 }
